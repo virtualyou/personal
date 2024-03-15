1eef49e29349c7cc9e04988ba0593aa5336c9bfa
David L Whitehurst <me@dlwhitehurst.com>
2024-03-14 21:02:20 -0400

    added swagger incomplete

--- src/app.ts
+++ src/app.ts
@@ -26,6 +26,10 @@ import peepRouter from "./routes/peep.routes";
 import cookieSession from "cookie-session";
 import * as process from "process";
 
+// swagger api documentation
+import swaggerUi from "swagger-ui-express";
+import swaggerDocument from "../swagger.json";
+
 const initIndex = process.argv.indexOf("--init=true");
 const init = initIndex !== -1;
 
@@ -58,6 +62,9 @@ app.use(function (_req, res, next) {
 app.get("/", (_req, res) => {
     res.send("Welcome to the VirtualYou Personal API.");
 });
+
+// swagger path to api documentation
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // database
 const Peep = db.peep;
